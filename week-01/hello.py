efb0fe66a20fc5d2124fd5a2e9f30418d7754425
Janis Zarans <janis.zarans@gmail.com>
2026-03-10 16:59:22 +0200

    feat: sync organized structure for week 1 and 2

---
+++ week-01/hello.py
@@ -0,0 +1,3 @@
+print ("Sveiki batareiki") # tipa standarta sākuma frāze laikam
+print (;bla bla bbla.) # Apzināti nepareiza sintakse, lai apskatītos, ko rāda termināli
+print ("ok, nu sanāk ierakstīt kaut ko")
